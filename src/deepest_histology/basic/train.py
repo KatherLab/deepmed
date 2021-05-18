d250eab1151bd43f67b0ac9ba2d6b45844fd077c
Marko van Treeck <mvantreeck@ukaachen.de>
2021-05-18 12:50:56 +0200

    Implement basic augmentations
    Resolves #15

--- src/deepest_histology/basic/train.py
+++ src/deepest_histology/basic/train.py
@@ -13,8 +13,9 @@ from sklearn import preprocessing
 from tqdm import tqdm
 
 from fastai.vision.all import (
-    Optimizer, Adam, Learner, DataBlock, ImageBlock, CategoryBlock, ColReader, ColSplitter, Resize,
-    resnet18, cnn_learner, BalancedAccuracy, SaveModelCallback, EarlyStoppingCallback, CSVLogger)
+    Optimizer, Adam, Learner, DataBlock, ImageBlock, CategoryBlock, ColReader, ColSplitter,
+    resnet18, cnn_learner, BalancedAccuracy, SaveModelCallback, EarlyStoppingCallback, CSVLogger,
+    aug_transforms)
 
 from ..utils import log_defaults
 
@@ -26,13 +27,14 @@ logger = logging.getLogger(__name__)
 def train(target_label: str, train_df: pd.DataFrame, result_dir: Path,
           arch: Callable[[bool], nn.Module] = resnet18,
           batch_size: int = 64,
-          image_size: int = 224,
           max_epochs: int = 10,
           opt: Optimizer = Adam,
           lr: float = 2e-3,
           patience: int = 3,
           num_workers: int = 0,
           device: torch.cuda._device_t = None,
+          tfms: Callable = aug_transforms(
+              flip_vert=True, max_rotate=360, max_zoom=1, max_warp=0, size=224),
           **kwargs) -> Learner:
 
     if device:
@@ -42,7 +44,7 @@ def train(target_label: str, train_df: pd.DataFrame, result_dir: Path,
                        get_x=ColReader('tile_path'),
                        get_y=ColReader(target_label),
                        splitter=ColSplitter('is_valid'),
-                       item_tfms=Resize(image_size))
+                       batch_tfms=tfms)
     dls = dblock.dataloaders(train_df, bs=batch_size, num_workers=num_workers)
 
     learn = cnn_learner(
