35952bf18033e15812ea44d8c93097d67d016a19
Marko van Treeck <markovantreeck@gmail.com>
2021-07-07 21:03:51 +0200

    Add examples

---
+++ examples/multi_target_crossval.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from deepest_histology.experiment_imports import *
+
+
+if __name__ == '__main__':
+    __spec__ = None
+    do_experiment(
+        project_dir=r'K:\Marko\BRCA_DX_TCGA_crossval',
+        get=partial(
+            get.multi_target,
+            get.crossval,
+            get.simple_run,
+            cohorts_df=cohort(
+                tile_path='D:/TCGA-BRCA-DX/BLOCKS_NORM_MACENKO',
+                clini_path='D:/BRCA_Docs/TCGA-BRCA-DX_CLINI.xlsx',
+                slide_path='D:/BRCA_Docs/TCGA-BRCA-DX_SLIDE_FULLNAMES.csv'),
+            target_labels=['ERStatus', 'PRStatus', 'HER2FinalStatus'],
+            max_tile_num=8,
+            valid_frac=.2,
+            na_values=['Not Available', 'Equivocal', 'Not Performed', 'Performed but Not Available']
+        ),
+        train=partial(
+            train,
+            batch_size=96,
+            max_epochs=1),
+        evaluator_groups=[
+            [partial(aggregate_stats, group_levels=[-3, -1])],
+            [aggregate_stats],
+            [Grouped(auroc), Grouped(count)]],
+        num_concurrent_runs=4)
---
+++ examples/multi_target_train.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from deepest_histology.experiment_imports import *
+
+
+if __name__ == '__main__':
+    __spec__ = None
+    do_experiment(
+        project_dir=r'K:\Marko\BRCA_DX_TCGA_full_training',
+        get=partial(
+            get.multi_target,
+            get.simple_run,
+            train_cohorts_df=cohort(
+                tile_path='D:/TCGA-BRCA-DX/BLOCKS_NORM_MACENKO',
+                clini_path='D:/BRCA_Docs/TCGA-BRCA-DX_CLINI.xlsx',
+                slide_path='D:/BRCA_Docs/TCGA-BRCA-DX_SLIDE_FULLNAMES.csv'),
+            target_labels=['ERStatus','PRStatus', 'HER2FinalStatus'],
+            max_tile_num=10,
+            valid_frac=.2,
+            na_values=['Not Available', 'Equivocal', 'Not Performed', 'Performed but Not Available']
+        ),
+        train=partial(
+            train,
+            batch_size=96,
+            max_epochs=20),
+        num_concurrent_runs=4)
