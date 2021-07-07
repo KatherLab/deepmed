1618f56f0d6386eae61717d5cd1a98274083b2f8
Marko van Treeck <markovantreeck@gmail.com>
2021-07-07 20:26:58 +0200

    Bump version number to v0.2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Marko van Treeck'
 author = 'Marko van Treeck'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
