58543dcedc7d110bd00195527d03d2dc469a7efd
Marko van Treeck <markovantreeck@gmail.com>
2021-08-16 13:29:28 +0200

    Fix missing imports

--- src/deepest_histology/__init__.py
+++ src/deepest_histology/__init__.py
@@ -6,10 +6,12 @@ from ._train import *
 from ._load import *
 from ._deploy import *
 
-__author__ = "Marko van Treeck"
-__copyright__ = "Copyright 2021, Kather Lab"
-__credits__ = ["Jakob Nikolas Kather", "Didem Cifci", "Oliver Lester Saldanha", "Marko van Treeck"]
-__license__ = "MIT"
-__version__ = "0.3.0"
-__maintainer__ = "Marko van Treeck"
-__email__ = "markovantreeck@gmail.com"
+__author__ = 'Marko van Treeck'
+__copyright__ = 'Copyright 2021, Kather Lab'
+__credits__ = [
+    'Jakob Nikolas Kather', 'Didem Cifci', 'Oliver Lester Saldanha', 'Marko van Treeck',
+    'Tobias Paul Seraphin', 'Céline Nicole Heinz', 'Chiara Loeffler', 'Hannah Sophie Muti']
+__license__ = 'MIT'
+__version__ = '0.4.0'
+__maintainer__ = 'Marko van Treeck'
+__email__ = 'markovantreeck@gmail.com'
--- src/deepest_histology/experiment_imports.py
+++ src/deepest_histology/experiment_imports.py
@@ -4,6 +4,8 @@ import coloredlogs
 coloredlogs.install(
     fmt='%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s', level=logging.INFO)
 
+from fastai.vision.all import *
+
 from pathlib import Path
 from functools import partial
 import pandas as pd
