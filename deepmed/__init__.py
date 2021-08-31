288da63f8b01c522669e904b0650f04499529a07
Marko van Treeck <markovantreeck@gmail.com>
2021-08-31 16:28:10 +0200

    Bump version number

--- deepmed/__init__.py
+++ deepmed/__init__.py
@@ -17,6 +17,6 @@ __credits__ = [
         'Hannah Sophie Muti',
     ]
 __license__ = 'MIT'
-__version__ = '0.6.dev0'
+__version__ = '0.6.0rc0'
 __maintainer__ = 'Marko van Treeck'
 __email__ = 'markovantreeck@gmail.com'
