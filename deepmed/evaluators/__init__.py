b4b72022344d61d5cf2ef57baee6b25b224cf0b5
Marko van Treeck <markovantreeck@gmail.com>
2021-09-07 11:28:22 +0200

    Also move metrics into evaluators

--- deepmed/evaluators/__init__.py
+++ deepmed/evaluators/__init__.py
@@ -3,5 +3,8 @@ from .heatmap import *
 from .roc import *
 from .adapters import *
 from .top_tiles import *
+from .metrics import *
 
-__all__ = ['Grouped', 'SubGrouped', 'aggregate_stats', 'roc', 'GroupMode', 'heatmap', 'top_tiles']
+__all__ = (
+    ['Grouped', 'SubGrouped', 'aggregate_stats', 'roc', 'GroupMode', 'heatmap', 'top_tiles'] +
+    metrics.__all__)
--- deepmed/experiment_imports.py
+++ deepmed/experiment_imports.py
@@ -12,7 +12,6 @@ from functools import partial
 import pandas as pd
 import deepmed
 from deepmed import *
-from deepmed import get, metrics, multi_input, evaluators
-from deepmed.metrics import *
+from deepmed import get, multi_input, evaluators
 from deepmed.evaluators import *
 from deepmed.get import cohort
