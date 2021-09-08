KatherLab/deepmed

commit fa55aff2b98a307ecddad819b587d2bc02c325f8

Fix for deployment for discretized targets in non-crossval

deepmed/_deploy.py
@@ -1,11 +1,13 @@
 import logging
+import re
 from typing import Optional
 
 import pandas as pd
 from fastai.vision.all import Learner, CategoryMap
+from typing import Iterable
 
 from .types import GPUTask
-from .utils import log_defaults
+from .utils import log_defaults, is_continuous
 
 __all__ = ['deploy']
 
@@ -23,15 +25,25 @@ def deploy(learn: Learner, task: GPUTask) -> Optional[pd.DataFrame]:
 
     test_df, target_label = task.test_df, task.target_label
 
-    test_dl = learn.dls.test_dl(test_df)
-    # inner needed so we don't jump GPUs
-    #FIXME What does `inner` actually _do_? Is this harmful?
-    scores, _, class_preds = learn.get_preds(dl=test_dl, inner=True, with_decoded=True)
-
-    # class-wise scores
     vocab = learn.dls.vocab
     if not isinstance(vocab, CategoryMap):
         vocab = vocab[-1]
+
+    test_df = _discretize_if_necessary(test_df=test_df, target_label=target_label, vocab=vocab)
+
+    # restrict testing classes to those known by the model
+    if not (known_idx := test_df[target_label].isin(vocab)).all():
+        unknown_classes = test_df[~known_idx].unique()
+        logger.warning(f'classes unknown to model in test data: {unknown_classes}!  Dropping them...')
+        test_df = test_df[known_idx]
+
+    test_dl = learn.dls.test_dl(test_df)
+    # inner needed so we don't jump GPUs
+    # FIXME What does `inner` actually _do_? Is this harmful?
+    scores, _, class_preds = learn.get_preds(
+        dl=test_dl, inner=True, with_decoded=True)
+
+    # class-wise scores
     for class_, i in vocab.o2i.items():
         test_df[f'{target_label}_{class_}'] = scores[:, i]
 
@@ -39,5 +51,31 @@ def deploy(learn: Learner, task: GPUTask) -> Optional[pd.DataFrame]:
     test_df[f'{target_label}_pred'] = vocab.map_ids(class_preds)
 
     test_df.to_csv(preds_path, index=False, compression='zip')
+
+    return test_df
+
+
+def _discretize_if_necessary(test_df: pd.DataFrame, target_label: str, vocab: Iterable[str]) -> pd.DataFrame:
+    # check if this target was discretized for training and discretize testing set if necessary
+    interval = re.compile(
+        r'^\[([+-]?\d+\.?\d*(?:e[+-]?\d+|)|-inf),([+-]?\d+\.?\d*(?:e[+-]?\d+|)|inf)\)$')
+    if is_continuous(test_df[target_label]) and \
+            all(interval.match(class_) is not None for class_ in vocab):
+
+        # extract thresholds from vocab
+        threshs = [*(interval.match(class_).groups()[0]  # type: ignore
+                     for class_ in vocab), 'inf']
+        threshs = sorted(threshs, key=float)
+
+        def interval_label(x):
+            """Discretizes data into ``[lower,upper)`` classes."""
+            for l, h in zip(threshs, threshs[1:]):
+                # we only transform the values here, because we want h, l to be
+                # *exactly* as in the training set
+                if float(l) <= x and x < float(h):
+                    return f'[{l},{h})'
+            raise RuntimeError('unreachable!')
+
+        test_df[target_label] = test_df[target_label].map(interval_label)
 
     return test_df

deepmed/get/_simple.py
@@ -167,14 +167,9 @@ def simple_run(
         elif test_cohorts_df is not None:
             logger.info(f'Searching for testing tiles')
             test_cohorts_df = _prepare_cohorts(
-                test_cohorts_df, target_label, na_values, n_bins, min_support=0, logger=logger)
+                test_cohorts_df, target_label, na_values, n_bins=None, min_support=0, logger=logger)
 
-            # restrict testing set to classes present in training set
-            if train_df is not None:
-                train_classes = train_df[target_label].unique()
-                test_cohorts_df = test_cohorts_df[test_cohorts_df[target_label].isin(train_classes)]
-
-            logger.info(f'Testing slide counts: {dict(test_cohorts_df[target_label].value_counts())}')
+            logger.info(f'Testing slide counts: {len(test_cohorts_df)}')
             test_df = _get_tiles(
                 cohorts_df=test_cohorts_df, max_tile_num=max_test_tile_num, seed=seed, logger=logger)
 
@@ -273,8 +268,8 @@ def _generate_train_df(
 
 
 def _prepare_cohorts(
-        cohorts_df: pd.DataFrame, target_label: str, na_values: Iterable[str], n_bins: int,
-        min_support: int, logger: logging.Logger) -> pd.DataFrame:
+        cohorts_df: pd.DataFrame, target_label: str, na_values: Iterable[str],
+        n_bins: Optional[int], min_support: int, logger: logging.Logger) -> pd.DataFrame:
     """Preprocesses the cohorts.
 
     Discretizes continuous targets and drops classes for which only few examples
@@ -292,7 +287,8 @@ def _prepare_cohorts(
         try:
             cohorts_df[target_label] = cohorts_df[target_label].map(float)
             logger.info(f'Discretizing {target_label}')
-            cohorts_df[target_label] = _discretize(cohorts_df[target_label].values, n_bins=n_bins)
+            if n_bins is not None:
+                cohorts_df[target_label] = _discretize(cohorts_df[target_label].values, n_bins=n_bins)
         except ValueError:
             pass
 
@@ -316,7 +312,7 @@ def _discretize(xs: Sequence[Number], n_bins: int) -> Sequence[str]:
                 # labels for intermediate classes
                 *(f'[{lower},{upper})'
                 for lower, upper in zip(est.bin_edges_[0][1:], est.bin_edges_[0][2:-1])),
-                f'[{est.bin_edges_[0][-2]}, inf)'] # label for largest class
+                f'[{est.bin_edges_[0][-2]},inf)'] # label for largest class
     label_map = dict(enumerate(labels))
     discretized = est.transform(unsqueezed).reshape(-1).astype(int)
     return list(map(label_map.get, discretized)) # type: ignore

deepmed/utils.py
@@ -2,8 +2,9 @@ import inspect
 import logging
 from typing import Callable, Any
 from functools import wraps, cached_property
+import pandas as pd
 
-__all__ = ['log_defaults', 'Lazy']
+__all__ = ['log_defaults', 'Lazy', 'is_continuous']
 
 
 def log_defaults(func):
@@ -49,3 +50,7 @@ class Lazy:
 
     def __setitem__(self, k, v):
         self._val[k] = v
+
+
+def is_continuous(series: pd.Series) -> bool:
+    return series.dtype == float

commit 49535a94fe9f001a466dd2a2eee472cbe782faaa

Bugfix
Regression from fa55aff2b98a307ecddad819b587d2bc02c325f8

deepmed/_deploy.py
@@ -34,7 +34,7 @@ def _deploy(learn: Learner, task: GPUTask) -> Optional[pd.DataFrame]:
 
     # restrict testing classes to those known by the model
     if not (known_idx := test_df[target_label].isin(vocab)).all():
-        unknown_classes = test_df[~known_idx].unique()
+        unknown_classes = test_df[target_label][~known_idx].unique()
         logger.warning(
             f'classes unknown to model in test data: {unknown_classes}!  Dropping them...')
         test_df = test_df[known_idx]

examples/multi_target_deploy.py
@@ -25,7 +25,7 @@ def main():
         get=get.MultiTarget(
             get.SimpleRun(),
             test_cohorts_df=test_cohorts_df,
-            target_labels=['ER Status By IHC'],
+            target_labels=['ER Status By IHC', 'TCGA Subtype'],
             max_test_tile_num=512,
             evaluators=[Grouped(auroc), Grouped(F1()), Grouped(count)],
             multi_target_evaluators=[AggregateStats(label='target')],
@@ -33,7 +33,8 @@ def main():
                 project_dir=project_dir,
                 training_project_dir='multi_target_train'),
         ),
-        devices={'cuda:0': 4}
+        devices={'cuda:0': 4},
+        num_concurrent_tasks=0,
     )
 
 

examples/multi_target_train.py
@@ -18,7 +18,7 @@ def main():
         get=get.MultiTarget(    # train for multiple targets
             get.SimpleRun(),
             train_cohorts_df=train_cohorts_df,
-            target_labels=['ER Status By IHC'],  # target labels to train for
+            target_labels=['ER Status By IHC', 'TCGA Subtype'],  # target labels to train for
             max_train_tile_num=128,  # maximum number of tiles per patient to train with
             max_valid_tile_num=128,  # maximum number of tiles per patient to validate with
             # amount of data to use as validation set (for early stopping)