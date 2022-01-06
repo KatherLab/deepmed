06c9c8b6bb9e63f26021904e179ca1527d248cb4
Marko van Treeck <markovantreeck@gmail.com>
2022-01-06 08:38:58 +0100

    Use threads instead of processes for dispatching tasks
    Linux doesn't like launching pytorch in different processes

--- deepmed/_experiment.py
+++ deepmed/_experiment.py
@@ -7,7 +7,9 @@ import traceback
 from typing import Mapping, Union, Optional
 from pathlib import Path
 from multiprocessing import Manager
-from multiprocessing.pool import ThreadPool
+import concurrent
+
+from fastcore.parallel import ThreadPoolExecutor
 
 from .types import *
 
@@ -61,57 +63,17 @@ def do_experiment(
             for device, capacity in devices.items()}
         tasks = get(project_dir=project_dir,
                     manager=manager, capacities=capacities)
-        num_concurrent_tasks = (sum(devices.values()) * 3
-                                if num_concurrent_tasks is None
-                                else num_concurrent_tasks)
 
-        # We use a ThreadPool which starts processes so our launched processes are:
-        #  1. Terminated after each training task so we don't leak resources
-        #  2. We can spawn more processes in the launched subprocesses (not possible with Pool)
-        with ThreadPool(num_concurrent_tasks or 1) as pool:
-            # only use pool if we actually want to task multiple tasks in parallel
-            # for loop to consume iterator
-            try:
-                for _ in (pool.imap_unordered(_task_wrapper, tasks, chunksize=1)
-                        if num_concurrent_tasks >= 1
-                        else (task.run() for task in tasks)):  # type: ignore
-                    pass
-            except Exception as e:
-                if not keep_going:
-                    raise e
-
-
-def _task_wrapper(task: Task) -> None:
-    """Starts a new process to train a model."""
-    p = ExceptionSavingProcess(target=task.run)
-    p.start()
-    p.join()
-    if p.exception:
-        error, traceback = p.exception
-        logging.getLogger(str(task.path)).exception(traceback)
-        raise error
-
-
-# Snippet by StackOverflow user mrkwjc: https://stackoverflow.com/a/33599967
-# Licensed under CC BY-SA 4.0
-class ExceptionSavingProcess(mp.Process):
-    def __init__(self, *args, **kwargs):
-        mp.Process.__init__(self, *args, **kwargs)
-        self._pconn, self._cconn = mp.Pipe()
-        self._exception = None
-
-    def run(self):
         try:
-            mp.Process.run(self)
-            self._cconn.send(None)
-        except Exception as e:
-            tb = traceback.format_exc()
-            self._cconn.send((e, tb))
-            #raise e
+            if num_concurrent_tasks == 0:
+                    for task in tasks:
+                        task.run()
+            else:
+                with ThreadPoolExecutor(num_concurrent_tasks) as e:
+                    futures = [e.submit(Task.run, task) for task in tasks]
+                    for future in concurrent.futures.as_completed(futures):
+                        future.result() # consume results to trigger exceptions
 
-    @property
-    def exception(self):
-        if self._pconn.poll():
-            self._exception = self._pconn.recv()
-        return self._exception
-# Snippet end
+        except Exception as e:
+            if not keep_going:
+                raise e
