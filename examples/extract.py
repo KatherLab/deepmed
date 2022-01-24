5607e5f9947647aed66f217a9e6f88ac59f38cb5
Marko van Treeck <markovantreeck@gmail.com>
2022-01-24 13:30:14 +0100

    Add feature extraction example

---
+++ examples/extract.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from deepmed.experiment_imports import *
+from deepmed.get._extract_features import Extract
+from deepmed.get._extract_features import PretrainedModel
+
+
+def main():
+    do_experiment(
+        project_dir='multi_target_train',
+        get=Extract(
+            feat_dir='/feature/output/dir',
+            tile_dir='/tile/dir',
+            arch=PretrainedModel('https://katherlab.s3.eu-central-1.amazonaws.com/dachs-moco-v2-best-XXXXXXXX.pt'),
+        ),
+    )
+
+
+if __name__ == '__main__':
+    main()
